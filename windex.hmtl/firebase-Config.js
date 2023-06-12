2a87a0a97a2209156c712e1863b9d89832202043
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-12 12:27:51 -0300

    pagina login

---
+++ windex.hmtl/firebase-Config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKKXMlW3AX5AMyubXHB-AZuSj4j59h2Kw",
+  authDomain: "blog-c29f6.firebaseapp.com",
+  databaseURL: "https://blog-c29f6-default-rtdb.firebaseio.com",
+  projectId: "blog-c29f6",
+  storageBucket: "blog-c29f6.appspot.com",
+  messagingSenderId: "618162704145",
+  appId: "1:618162704145:web:b0292c20c706bee445c6b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
