5ba014cc87c13fa2d652c0b1b3403a04e0d8079a
15521Mib <131001184+15521Mib@users.noreply.github.com>
2023-08-02 10:04:02 -0300

    codigos

---
+++ revat2/revat2/exemplo1.js
@@ -0,0 +1,16 @@
+const leia = require("prompt-sync")()
+
+
+let nomes = []
+
+nomes[0] = "Giovana"
+nomes[1] = 16
+nomes[10] = {
+    nome : "Vitoria",
+    idade : 16
+}
+
+console.log(nomes)
+console.log(nomes[4])
+console.log(nomes[10])
+console.log(nomes.leght)
